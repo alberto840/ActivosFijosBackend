d237bdfb2ed20b6709b9010c5030c28216763292
Alberto Flores <alberto.flores@ucb.edu.bo>
2024-10-24 12:39:51 -0400

    probando con otra solucion cors

--- src/main/java/com/grupod/activosfijos/config/CorsConfig.java
+++ src/main/java/com/grupod/activosfijos/config/CorsConfig.java
@@ -14,16 +14,22 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
+        // Orígenes permitidos
         config.setAllowedOrigins(Arrays.asList(
             "http://localhost:4200", 
-            "https://jhosep2022.github.io" 
+            "https://jhosep2022.github.io"
         ));
+        // Métodos permitidos
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        // Cabeceras permitidas
         config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        // Permitir que se envíen cookies (autenticación, etc.)
         config.setAllowCredentials(true);
+        // Asegurarse de permitir la cabecera 'Access-Control-Allow-Origin'
+        config.addExposedHeader("Access-Control-Allow-Origin");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/", config);
+        source.registerCorsConfiguration("/**", config);
 
         return new CorsFilter(source);
     }
